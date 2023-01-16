7eb743d101d0ce4e597ceb2ba4d1e13b5b88316b
netanelklein <netanel@netanelk.com>
2023-01-16 21:45:17 +0200

    Added 'provider' for app state managment

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 import 'firebase_options.dart';
 import 'style/color_schemes.dart';
 import 'screens/add_image.dart';
